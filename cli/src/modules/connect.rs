3dcd91064146e6acf13754b2d784804c0476b424
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-09-17 04:45:20 +0300

    wip - update connect options to match local WRS

--- cli/src/modules/connect.rs
+++ cli/src/modules/connect.rs
@@ -10,7 +10,7 @@ impl Connect {
         let url = argv.first().cloned().or_else(|| ctx.wallet().settings().get(WalletSettings::Server));
         let network_type = ctx.wallet().network_id()?;
         let url = ctx.wallet().rpc_client().parse_url_with_network_type(url, network_type.into()).map_err(|e| e.to_string())?;
-        let options = ConnectOptions { block_async_connect: true, strategy: ConnectStrategy::Fallback, url, timeout: None };
+        let options = ConnectOptions { block_async_connect: true, strategy: ConnectStrategy::Fallback, url, ..Default::default() };
         ctx.wallet().rpc_client().connect(options).await.map_err(|e| e.to_string())?;
         Ok(())
     }
