a43b78f08a25663e48518fc9de71213c51ee336e
Surinder Singh Matoo <surinder83singh@gmail.com>
2024-03-16 04:27:58 +0530

    simple session for http requests

--- wasm/examples/browser-extension/server.js
+++ wasm/examples/browser-extension/server.js
@@ -5,25 +5,149 @@ const http = require('http');
 const {URL} = require('url');
 const path = require('path');
 const fs = require('fs');
+const qs = require('querystring');
 
 const port = process.argv[2] || "8000";
+
+const USERS = {
+    xyz: "xyz"
+}
+let sessions = {};
 
 // Colors for CLI output
 const WHT = '\x1B[39m';
 const RED = "\x1B[91m";
 const GRN = "\x1B[32m";
 
+readSession();
 
 http.createServer(function (req, res) {
+    let userSession = getUserSession(req);
+
+
     let url = req.url == "/" ? "/index.html": req.url;
 
     let pathname = new URL(url, "http://localhost").pathname;
 
     if (pathname.startsWith("/api/")){
-        handleApiRequest(pathname.replace("/api/", ""), req, res)
+        handleApiRequest(pathname.replace("/api/", ""), userSession, req, res)
         return
     }
 
+    if (pathname == "/logout"){
+        deleteUserSession(req);
+        return redirectTo(res, "/")
+    }
+
+    if (pathname == "/login.html" && req.method == "POST"){
+        let body = "";
+        req.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const info = qs.parse(body);
+            if (USERS[info.username] == info.password){
+                const sessionId = generateSessionId();
+
+                
+                sessions[sessionId] = {
+                    username: info.username,
+                    loggedIn: true
+                };
+
+                saveSession()
+
+                // Set session ID as a cookie
+                res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly`);
+                res.writeHead(302, { 'Location': '/' });
+                res.end();
+                return
+            }
+
+            send("login.html", {error:"Invalid username or password"});
+        })
+        
+        return
+    }
+
+    if (!userSession 
+        && !pathname.startsWith("/index.html")
+        && !pathname.startsWith("/resources")
+        && !pathname.startsWith("/login")){
+        return redirectTo(res, '/login.html')
+    }
+
+    // update session data and save it
+    // userSession.abc = "xyz";
+    // saveSession()
+
+    send(pathname);
+
+    function send(pathname, data={}){
+        if (userSession){
+            data.username = userSession.username;
+            data.user_greeting_msg = "Hello: "+data.username;
+        }
+        sendFile(pathname, data, req, res);
+    }
+
+}).listen(parseInt(port, 10));
+
+
+function generateSessionId() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+function redirectTo(res, loc) {
+    res.writeHead(302, { 'Location': loc  });
+    res.end();
+}
+
+function saveSession(){
+    fs.writeFileSync("session.json", JSON.stringify(sessions, null, "\t"));
+}
+function readSession(){
+    fs.stat("session.json", (err, )=>{
+        if (!err){
+            sessions = JSON.parse(fs.readFileSync("session.json")+"");
+        }
+    });
+    
+}
+
+function getSessionId(req){
+    const cookies = req.headers.cookie;
+    let sessionId;
+
+    if (cookies) {
+        const cookieParts = cookies.split(';').map(cookie => cookie.trim().split('='));
+        const sessionCookie = cookieParts.find(cookie => cookie[0] === 'sessionId');
+
+        if (sessionCookie) {
+            sessionId = sessionCookie[1];
+        }
+    }
+    return sessionId
+}
+
+function getUserSession(req){
+    let sessionId = getSessionId(req);
+
+    if (sessionId && sessions[sessionId] && sessions[sessionId].loggedIn) {
+        return sessions[sessionId]
+    }
+    return false
+}
+function deleteUserSession(req){
+    let sessionId = getSessionId(req);
+    if (sessionId && sessions[sessionId]){
+        delete sessions[sessionId];
+        saveSession()
+    }
+}
+
+function sendFile(pathname, data, req, res){
     // get the /file.html from above and then find it from the current folder
     let filename = path.join(process.cwd(), pathname);
 
@@ -53,7 +177,7 @@ http.createServer(function (req, res) {
         }
 
         // Assuming the file exists, read it
-        fs.readFile(filename, 'binary', function (err, file) {
+        fs.readFile(filename, 'utf8', function (err, content) {
             // Output a green line to console explaining the file that will be loaded in the browser
             console.log(GRN + 'FILE: ' + WHT + pathname);
             // If there was an error trying to read the file
@@ -70,21 +194,44 @@ http.createServer(function (req, res) {
                 res.setHeader('Content-Type', contentType);
             }
 
+            if (data.username){
+                content = content.replace(/<IF-USER>([\s\S]+?)<\/IF-USER>/g, (match, code) => {
+                    return code
+                });
+                content = content.replace(/<IF-NOT-USER>([\s\S]+?)<\/IF-NOT-USER>/g, (match, code) => {
+                    return ""
+                });
+            }else{
+                content = content.replace(/<IF-USER>([\s\S]+?)<\/IF-USER>/g, (match, code) => {
+                    return ""
+                });
+                content = content.replace(/<IF-NOT-USER>([\s\S]+?)<\/IF-NOT-USER>/g, (match, code) => {
+                    return code
+                });
+            }
+            content = content.replace(/<%=([\s\S]+?)%>/g, (match, code) => {
+                return data[code.trim()]??"";
+            });
+
             // Output the read file to the browser for it to load
             res.writeHead(200);
-            res.write(file, 'binary');
+            res.write(content);
             res.end();
         });
 
     });
+}
 
-}).listen(parseInt(port, 10));
-
-function handleApiRequest(endpoint, req, res){
+function handleApiRequest(endpoint, userSession, req, res){
     res.setHeader('Content-Type', 'text/plain');
+    //console.log("req", req)
     switch (endpoint){
         case "check-payment":{
-            res.write("TODO");
+            if (!userSession){
+                res.write("please login first");
+            }else{
+                res.write("TODO");
+            }
         }break;
         default:{
             res.writeHead(500);
