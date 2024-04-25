bedc1cc4c83428d5c15da74a841ac21982f74f3e
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-04-24 21:49:31 -0400

    lints

--- cli/src/modules/connect.rs
+++ cli/src/modules/connect.rs
@@ -16,17 +16,20 @@ impl Connect {
                 Some("public") => {
                     tprintln!(ctx, "Connecting to a public node");
                     Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url
-                },
+                }
                 None => {
                     tprintln!(ctx, "No server set, connecting to a public node");
                     Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url
-                },
-                Some(url) => {
-                    wrpc_client.parse_url_with_network_type(url.to_string(), network_id.into()).map_err(|e| e.to_string())?
-                },
+                }
+                Some(url) => wrpc_client.parse_url_with_network_type(url.to_string(), network_id.into()).map_err(|e| e.to_string())?,
             };
 
-            let options = ConnectOptions { block_async_connect: true, strategy: ConnectStrategy::Fallback, url : Some(url), ..Default::default() };
+            let options = ConnectOptions {
+                block_async_connect: true,
+                strategy: ConnectStrategy::Fallback,
+                url: Some(url),
+                ..Default::default()
+            };
             wrpc_client.connect(Some(options)).await.map_err(|e| e.to_string())?;
         } else {
             terrorln!(ctx, "Unable to connect with non-wRPC client");
