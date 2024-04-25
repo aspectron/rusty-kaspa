f650281fd056bbd6577f2490e2688aead07d9fe6
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-04-24 21:35:40 -0400

    support public resolver in CLI (default server now is 'public')

--- cli/src/modules/connect.rs
+++ cli/src/modules/connect.rs
@@ -1,4 +1,5 @@
 use crate::imports::*;
+use kaspa_wrpc_client::Resolver;
 
 #[derive(Default, Handler)]
 #[help("Connect to a Kaspa network")]
@@ -9,11 +10,23 @@ impl Connect {
         let ctx = ctx.clone().downcast_arc::<KaspaCli>()?;
         if let Some(wrpc_client) = ctx.wallet().try_wrpc_client().as_ref() {
             let url = argv.first().cloned().or_else(|| ctx.wallet().settings().get(WalletSettings::Server));
-            let network_type = ctx.wallet().network_id()?;
-            let url = url
-                .map(|url| wrpc_client.parse_url_with_network_type(url, network_type.into()).map_err(|e| e.to_string()))
-                .transpose()?;
-            let options = ConnectOptions { block_async_connect: true, strategy: ConnectStrategy::Fallback, url, ..Default::default() };
+            let network_id = ctx.wallet().network_id()?;
+
+            let url = match url.as_deref() {
+                Some("public") => {
+                    tprintln!(ctx, "Connecting to a public node");
+                    Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url
+                },
+                None => {
+                    tprintln!(ctx, "No server set, connecting to a public node");
+                    Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url
+                },
+                Some(url) => {
+                    wrpc_client.parse_url_with_network_type(url.to_string(), network_id.into()).map_err(|e| e.to_string())?
+                },
+            };
+
+            let options = ConnectOptions { block_async_connect: true, strategy: ConnectStrategy::Fallback, url : Some(url), ..Default::default() };
             wrpc_client.connect(Some(options)).await.map_err(|e| e.to_string())?;
         } else {
             terrorln!(ctx, "Unable to connect with non-wRPC client");
