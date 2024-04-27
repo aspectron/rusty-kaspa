f242c8dfa6b5a68e800b66d98e813d686538c725
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-04-26 23:36:12 -0400

    Update connect command to print a notice when connecting to a public node. Rename the local url variable.

--- cli/src/modules/connect.rs
+++ cli/src/modules/connect.rs
@@ -9,20 +9,32 @@ impl Connect {
     async fn main(self: Arc<Self>, ctx: &Arc<dyn Context>, argv: Vec<String>, _cmd: &str) -> Result<()> {
         let ctx = ctx.clone().downcast_arc::<KaspaCli>()?;
         if let Some(wrpc_client) = ctx.wallet().try_wrpc_client().as_ref() {
-            let url = argv.first().cloned().or_else(|| ctx.wallet().settings().get(WalletSettings::Server));
             let network_id = ctx.wallet().network_id()?;
 
-            let url = match url.as_deref() {
+            let arg_or_server_address = argv.first().cloned().or_else(|| ctx.wallet().settings().get(WalletSettings::Server));
+            let (is_public, url) = match arg_or_server_address.as_deref() {
                 Some("public") => {
                     tprintln!(ctx, "Connecting to a public node");
-                    Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url
+                    (true, Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url)
                 }
                 None => {
                     tprintln!(ctx, "No server set, connecting to a public node");
-                    Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url
+                    (true, Resolver::default().fetch(WrpcEncoding::Borsh, network_id).await.map_err(|e| e.to_string())?.url)
                 }
-                Some(url) => wrpc_client.parse_url_with_network_type(url.to_string(), network_id.into()).map_err(|e| e.to_string())?,
+                Some(url) => {
+                    (false, wrpc_client.parse_url_with_network_type(url.to_string(), network_id.into()).map_err(|e| e.to_string())?)
+                }
             };
+
+            if is_public {
+                tpara!(
+                    ctx,
+                    "Please note that default public nodes are community-operated and \
+                    accessing them may expose your IP address to different node providers. \
+                    Consider running your own node for better privacy. \
+                    ",
+                );
+            }
 
             let options = ConnectOptions {
                 block_async_connect: true,
