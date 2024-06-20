74fa380a9c4a2f37527686ae5ea3cf599ea28688
cdvallone <cvallone28@gmail.com>
2024-06-20 11:54:28 -0400

    Updated note

--- wallet/keys/src/privkeygen.rs
+++ wallet/keys/src/privkeygen.rs
@@ -49,7 +49,8 @@ impl PrivateKeyGenerator {
 impl PrivateKeyGenerator {
     #[new]
     pub fn new_py(xprv: String, is_multisig: bool, account_index: u64, cosigner_index: Option<u32>) -> Result<PrivateKeyGenerator> {
-        // NOTE: accepting xprv as String instead of XPrvT due to challenges with XPrvT type when building python interface
+        // NOTE: accepting `xprv: String`` instead of `xprv: XPrvT`` due to challenges with XPrvT type when building python bindings
+        // Seems like Python constructor fns will need to be in their own block anyways. Maybe opportunity to share code and wrap it for both?
         let xprv = XPrv::from_xprv_str(xprv)?;
         let xprv = xprv.inner();
         let receive = xprv.clone().derive_path(&WalletDerivationManager::build_derivate_path(
