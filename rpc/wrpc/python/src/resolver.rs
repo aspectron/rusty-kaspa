020cbdb2fcb66ffe707edecbf67c9850f482beb3
cdvallone <cvallone28@gmail.com>
2024-07-16 10:28:41 -0400

    rpc.py example tn11

--- rpc/wrpc/python/src/resolver.rs
+++ rpc/wrpc/python/src/resolver.rs
@@ -5,8 +5,6 @@ use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use std::{str::FromStr, sync::Arc};
 
-use crate::client::RpcClient;
-
 #[derive(Debug, Clone)]
 #[pyclass]
 pub struct Resolver {
@@ -63,23 +61,7 @@ impl Resolver {
         }}
     }
 
-    fn connect(
-        &self,
-        py: Python,
-        encoding: Option<String>,
-        network: Option<String>,
-        network_suffix: Option<u32>,
-    ) -> PyResult<Py<PyAny>> {
-        let encoding = WrpcEncoding::from_str(encoding.unwrap_or(String::from("borsh")).as_str()).unwrap();
-        let network_id = into_network_id(&network.unwrap(), network_suffix)?;
-        let client = RpcClient::new(Some(self.clone()), None, Some(encoding), Some(network_id))?;
-
-        let client = client.clone();
-        py_async! {py, async move {
-            let _ = client.connect(Some(options)).await.map_err(|e| pyo3::exceptions::PyException::new_err(e.to_string()));
-            Ok(())
-        }}
-    }
+    // fn connect() TODO
 }
 
 impl From<Resolver> for NativeResolver {
