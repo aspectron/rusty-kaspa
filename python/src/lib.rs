80dcbec1659bbaaa1ec0e73488c8ab1c7e7fbf04
cdvallone <cvallone28@gmail.com>
2024-07-28 07:09:57 -0400

    PublicKeyGenerator

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -10,6 +10,7 @@ cfg_if::cfg_if! {
             m.add_class::<kaspa_wallet_keys::privkeygen::PrivateKeyGenerator>()?;
             m.add_class::<kaspa_wallet_keys::privatekey::PrivateKey>()?;
             m.add_class::<kaspa_wallet_keys::publickey::PublicKey>()?;
+            m.add_class::<kaspa_wallet_keys::pubkeygen::PublicKeyGenerator>()?;
 
             m.add_class::<kaspa_wrpc_python::client::RpcClient>()?;
             m.add_class::<kaspa_wrpc_python::resolver::Resolver>()?;
